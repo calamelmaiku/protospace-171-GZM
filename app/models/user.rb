f6dfe42722b7a29432682c98ad060ae7bb7ce1eb
maesuke0401 <166385799+maesuke0401@users.noreply.github.com>
2024-05-09 18:32:01 +0900

    ユーザーモデル作成とバリテーション

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+         validates :name, presence: true
+         validates :profile, presence: true
+         validates :occuption, presence: true
+         validates :position, presence: true
+  end
