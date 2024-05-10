8865e7b5b0c8c85f72ef2ae09aad53066a2cd9ad
calamelmaiku <bokumike.caramelpudding@gmail.com>
2024-05-10 10:43:25 +0900

    Prototypeモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_05_09_074927) do
+ActiveRecord::Schema[7.0].define(version: 2024_05_10_013727) do
+  create_table "prototypes", charset: "utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "catch_copy", null: false
+    t.text "concept", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_prototypes_on_user_id"
+  end
+
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.text "profile", null: false
