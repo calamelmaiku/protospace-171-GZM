f2d96e234b02920d200f09641d699f05104f48a7
ricky1423 <rikisoccer0904@gmail.com>
2024-05-10 18:35:52 +0900

    詳細ページ 修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -2,6 +2,7 @@ class PrototypesController < ApplicationController
   def index
     @prototypes = Prototype.all
   end
+
   def show
     @prototype = Prototype.find(params[:id])
     render :show, locals: { prototype: @prototype }
