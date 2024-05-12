3b8335904b9e49b2359f960682f680a88c2b39a6
calamelmaiku <bokumike.caramelpudding@gmail.com>
2024-05-12 11:21:18 +0900

    コメントテーブルモデル作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :prototypes
+  has_meny :comments
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
