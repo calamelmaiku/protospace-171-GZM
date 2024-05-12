0a2f23da989e1321ca6c5febdc4b8ec04e6e04c0
calamelmaiku <bokumike.caramelpudding@gmail.com>
2024-05-12 12:58:32 +0900

    コメント表示

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -6,6 +6,7 @@ class PrototypesController < ApplicationController
   def show
     @prototype = Prototype.find(params[:id])
     @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
     render :show, locals: { prototype: @prototype, comment: @comment }
   end
 
