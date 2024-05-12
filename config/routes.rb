26aed52eb98657a9298251825cde460738bdefc1
calamelmaiku <bokumike.caramelpudding@gmail.com>
2024-05-12 10:10:19 +0900

    ルートコントローラー設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
    devise_for :users
    root to:  "prototypes#index"
-   resources :prototypes, only: [:index, :new, :create]
+   resources :prototypes, only: [:index, :new, :create, :edit, :update]
   # Defines the root path route ("/")
   # root "articles#index"
 
